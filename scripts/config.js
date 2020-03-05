0a59ea558f2a6deab3e209140e4afe3c16dcfa84
liwuxi <imwuxi@gmail.com>
2020-03-05 23:29:53 +0800

    first commit

---
+++ scripts/config.js
@@ -0,0 +1,15 @@
+hexo.on('generateBefore', function () {
+    var rootConfig = hexo.config;
+  
+    if (hexo.locals.get) {
+      // 获取 source/_data 目录下的文件
+      var data = hexo.locals.get('data');
+  
+      // 如果存在 stun.yml 文件，就用它覆盖原来的配置文件
+      if (data && data.stun) {
+        hexo.theme.config = data.escape;
+      }
+    }
+  
+    hexo.theme.config.rootConfig = rootConfig;
+  });
