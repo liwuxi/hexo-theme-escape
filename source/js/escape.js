e9d68afbec61c7e476d4806a31adf7d22559bc1f
liwuxi <imwuxi@gmail.com>
2020-03-14 16:12:29 +0800

    Complete overall layout

---
+++ source/js/escape.js
@@ -0,0 +1,20 @@
+/**
+ * Hexo-theme-escape
+ * @author Andy Lee
+ * @date 2020-3-10
+ */
+
+// $('body').on('click', function(e) {
+//   switch(true) {
+//     case (tag.indexOf('more') != -1):
+//       var num = parsetInt(tag.data('page')) || 1;
+//       if(num == 1) {
+//         tag.data('page', 1);
+//       }
+//       if(num >= Pages) {
+//         return;
+//       }
+//       tag.html(tag.attr('data-loading')).data('status', 'loading');
+      
+//   }
+// })
