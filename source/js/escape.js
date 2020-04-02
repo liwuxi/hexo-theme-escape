9fe8efd3397c57f18348c7dfa2455329a215a22e
liwuxi <imwuxi@gmail.com>
2020-04-02 11:21:32 +0800

    Feat: add the go up button & update resource file

--- source/js/escape.js
+++ source/js/escape.js
@@ -4,6 +4,19 @@
  * @date 2020-3-10
  */
 
-// $(function() {
-//     // console.log(window);
-// })
+$(function() {
+    // Go up button
+    $("#go-up").hide();
+    var height = $(window).height();
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > height) {
+            $("#go-up").fadeIn();
+        }else{ 
+            $("#go-up").fadeOut();
+        }
+    });
+    $("#go-up").click(function() {
+        $("html, body").animate({scrollTop: 0}, 800);
+        return false;
+    });
+})
