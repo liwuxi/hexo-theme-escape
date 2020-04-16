b736bc457960c9d3dbd646e7a1ffea9f14cfd227
liwuxi <imwuxi@gmail.com>
2020-04-16 16:16:56 +0800

    Feat: adding a topic word feature to an article

--- source/js/escape.js
+++ source/js/escape.js
@@ -7,16 +7,26 @@
 $(function() {
     // Go up button
     $("#go-up").hide();
-    var height = $(window).height();
+    let height = $(window).height();
     $(window).scroll(function() {
         if ($(window).scrollTop() > height) {
             $("#go-up").fadeIn();
-        }else{ 
+        } else { 
             $("#go-up").fadeOut();
         }
     });
     $("#go-up").click(function() {
         $("html, body").animate({scrollTop: 0}, 800);
         return false;
+    });
+
+    // TOC
+    let tocTop = $("#article-toc").offset().top;
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > tocTop) {
+            $(".article-toc").addClass("fixed");
+        } else {
+            $(".article-toc").removeClass("fixed");
+        }
     });
 })
